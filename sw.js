6395b98bf1a5b818da20e3ccb88889b7e009446a
pepecuaik <pepecuaik@gmail.com>
2026-08-31 16:45:22 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cuaik-reservas-v2";
+const CACHE_NAME = "cuaik-reservas-v3";
 const SHELL = ["/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
